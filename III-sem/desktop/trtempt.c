9556bbd3d4442a0d9ae5c764d3e4d84d449f77c5
wasit shafi <wasitshafi700@gmail.com>
2019-08-24 00:50:38 +0530

    removed .class files

--- III-sem/desktop/trtempt.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-int main()
-{
-    double num = 6, squareRoot;
-    squareRoot =  sqrt(num);
-    printf("Square root of %lf =  %lf", num, squareRoot);
-    return 0;
-}
