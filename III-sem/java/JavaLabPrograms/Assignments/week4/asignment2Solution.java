7a11d9b222c7e53ebaa07cb46cd49ab67e0c42df
wasit shafi <wasitshafi700@gmail.com>
2019-09-04 07:10:09 +0530

    added files

--- III-sem/java/JavaLabPrograms/Assignments/week4/asignment2Solution.java
+++ III-sem/java/JavaLabPrograms/Assignments/week4/asignment2Solution.java
@@ -33,8 +33,7 @@ class Graph
     {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Press 1 if V(Vi, Vj) adjacent vertex otherwise 0.\n\n");
-
+ 
         for(int i = 0 ; i < adjacencyMatrix.length ; i++)
         {
             for(int j = 0 ; j < adjacencyMatrix.length ; j++)
@@ -43,7 +42,7 @@ class Graph
                     adjacencyMatrix[i][j] = 0;
                 else
                 {
-                    System.out.print("Is V(" + (i + 1) + ", " + (j + 1) + ") adjacent : ");
+                    System.out.print("V(" + (i + 1) + ", " + (j + 1) + ") : ");
                     adjacencyMatrix[i][j] = scan.nextInt();
                 }
             }
@@ -61,10 +60,26 @@ class Graph
 		}
 	}
 
-	public void isConnected()
+	public boolean isConnected()
     {  
-		System.out.println("isConnected");    	
-	}
+        int temp[][] = new int[adjacencyMatrix.length][adjacencyMatrix.length];
+            // copy 
+        for(int i = 0 ; i < temp.length ; i++)
+            for(int j = 0 ; j < temp.length ; j++)
+                temp[i][j] = adjacencyMatrix[i][j];
+
+        for(int k = 0 ; k < temp.length ; k ++)
+            for(int i = 0 ; i < temp.length ; i++)
+                for(int j = 0 ; j < temp.length ; j++)
+                    if(temp[i][k] == 1 && temp[k][j] == 1)
+                        temp[i][j] = 1;
+   
+        for(int i = 0 ; i < temp.length ; i++)
+             for(int j = 0 ; j < temp.length ; j++)
+                    if(temp[i][j] == 0)
+                        return false; 
+          return true;
+    }
 }
 
 class DirectedGraph extends Graph
@@ -79,35 +94,35 @@ class DirectedGraph extends Graph
         super(n);
     }
 
-	public void indegree()
+	public int[] indegree()
 	{
-        int sum;
+        int sum, degree[] = new int[adjacencyMatrix.length];
 
 		for(int i = 0 ; i < adjacencyMatrix.length; i++)
 		{
-			System.out.print("In-degree  of vertex " + (i + 1) + " : " );
 			sum = 0;
-
             for( int j = 0 ; j < adjacencyMatrix.length; j++)
-				sum = sum + adjacencyMatrix[i][j]; //sum of rows 
-		    
-		    System.out.println(sum);
-		}
+                sum += adjacencyMatrix[j][i];                //sum of cols
+            degree[i] = sum;
+        }
+        
+        return degree;
     }
     
-	public void outdegree()
-    {
-        int sum;
+	
+	public int[] outdegree()
+	{
+        int sum, degree[] = new int[adjacencyMatrix.length];
 
 		for(int i = 0 ; i < adjacencyMatrix.length; i++)
 		{
-			System.out.print("Out-degree of vertex " + (i + 1) + " : " );
 			sum = 0;
-			for( int j = 0 ; j < adjacencyMatrix.length; j++ )
-				sum = sum + adjacencyMatrix[j][i]; //sum of cols 
-		    System.out.println(sum);
-		}
-	}
+            for( int j = 0 ; j < adjacencyMatrix.length; j++)
+                sum += adjacencyMatrix[i][j];                 //sum of rows
+            degree[i] = sum;
+        }   
+        return degree;
+    }
 }
 
 public class asignment2Solution
@@ -116,16 +131,56 @@ public class asignment2Solution
 	{
 		Scanner scan = new Scanner(System.in);
 		DirectedGraph directedGraph;
-		int n;
+        int n;
+        int degree[];
 
-		System.out.print("Enter no. of vertices : ");
-		n = scan.nextInt();
-
-		directedGraph = new DirectedGraph(n);
+        // Directed Grap
+        System.out.println("****************");
+        System.out.println("*DIRECTED GRAPH*");
+        System.out.println("****************\n");
+		
+        System.out.print("Enter no. of vertices : ");
+        n = scan.nextInt();
+        
+        degree = new int[n];
+        
+        directedGraph = new DirectedGraph(n);
+        System.out.println("Press 1 iff V(i, j) adjacent otherwise 0:\n");
         directedGraph.input();
 
-        directedGraph.indegree();
+        System.out.println("Adjacency Matrix is as follows : ");
+        directedGraph.display();
+
         System.out.println();
-		directedGraph.outdegree();
+        degree = directedGraph.indegree();
+		for( int i = 0 ; i < degree.length; i++ ) System.out.println("In-degree  of vertex " + (i + 1) + " : " + degree[i] );
+
+        System.out.println();
+        degree = directedGraph.outdegree();
+		for( int i = 0 ; i < degree.length; i++ ) System.out.println("Out-degree of vertex " + (i + 1) + " : " + degree[i] );
+
+        System.out.println();
+        if(directedGraph.isConnected())
+           System.out.println("Graph is connected");
+        else
+           System.out.println("Graph is not connected");
+
+
+/*
+        System.out.println("****************");
+        System.out.println("*WEIGHTED GRAPH*");
+        System.out.println("****************\n");
+
+        System.out.print("Enter no. of vertices : ");
+        n = scan.nextInt();
+        
+        
+        weightedGraph = new WeightedGraph(n);
+        System.out.println("Press W(V(i, j)) iif they are adjacent otherwise -1:\n");
+        weightedGraph.input();
+
+        System.out.println("Adjacency Matrix is as follows : ");
+        weightedGraph.display();
+*/
 	}
 }
