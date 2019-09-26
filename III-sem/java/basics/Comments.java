346a7acdd17dfc4a49cf3698cd6f0da08fab523c
wasit shafi <wasitshafi700@gmail.com>
2019-09-27 00:08:59 +0530

    added files

---
+++ III-sem/java/basics/Comments.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Comments
+{
+    public static void main(Strings []args)
+    {
+        
+        /**
+         * This kind of comment is used to generate documentation automatically
+         * @author wasitshafi
+         * @version 10.2.5
+         * @since 2019-09-26
+         * 
+         * 
+         * for info refer : https://www.tutorialspoint.com/java/java_documentation.htm
+         * 
+         */
+
+         /*
+            This is multiline comment
+         */
+
+         // This is single line comment
+    }
+}
