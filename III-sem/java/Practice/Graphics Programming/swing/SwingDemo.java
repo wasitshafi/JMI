b9d5f3188fafe057cd8981bb432f68a2dd4bd7e4
wasit shafi <wasitshafi700@gmail.com>
2020-02-20 00:36:16 +0530

    added swing prog.

--- III-sem/java/Practice/Graphics Programming/swing/SwingDemo.java
+++ III-sem/java/Practice/Graphics Programming/swing/SwingDemo.java
@@ -1,58 +1,60 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+/**
+    @author wasitshafi
+    @since  18-02-2020
+*/
+// Refer : horstman pg no : 543 - 548
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame; //'x' denotes swing belongs to extended version not from core java
 
-
-class TestFrame extends JFrame implements ActionListener
+class TestFrame extends JFrame
 {
-    JLabel label1, label2, lblAns;
-    JButton btadd, btsub;
-    JTextField txtf1, txtf2;
-
-    TestFrame()
+    public TestFrame()
     {
-        setVisible(true);
-        setSize(400, 400);
-        setLayout(new FlowLayout());
+        Image icon = new ImageIcon("javaIcon.png").getImage();
+        
+        setIconImage(icon);
+        setTitle("This is frame Title");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // By default frame is hidded when user clicks on close even while closing the last frame opened / we can also exit using 'System.exit()' method
+        setResizable(false); // by default its true
 
-        label1 = new JLabel("Enter number1 : ");
-        label2 = new JLabel("Enter number2 : ");
-        lblAns = new JLabel("");
-        txtf1 = new JTextField(20); // size of textbox
-        txtf2 = new JTextField(20); // size of textbox
-        btadd = new JButton("ADD");
-        btsub = new JButton("SUB");
-        btadd.addActionListener(this);
-        btsub.addActionListener(this);
-
-        add(label1);
-        add(txtf1);
-        add(label2);
-        add(txtf2);
-        add(btadd);
-        add(btsub);
-
-        add(lblAns);
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-
-    public void actionPerformed(ActionEvent ae)
-    {
-        int num1 = Integer.parseInt(txtf1.getText());
-        int num2 = Integer.parseInt(txtf2.getText());
-        if(ae.getSource() == btadd)
-            lblAns.setText((num1 + num2) + ""); 
-        else if(ae.getSource() == btsub)
-            lblAns.setText((num1 - num2) + ""); 
-
+        //setSize(500,500); //Default size id 0 by 0 pixels
+        //setLocation(450, 10);// or setBound(x, y, width, height);
+        //setLocationByPlatform(true); // It usually picks the location with a slight offset from the last window
+              
+        //setVisible(true); // Its by default false because after creating frame we can add components to it and later set its visibility true
     }
 }
 public class SwingDemo
 {
     public static void main(String... args)
     {
-        TestFrame obj = new TestFrame();
+        TestFrame f = new TestFrame();
 
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension screenSize = tk.getScreenSize();
+        int width  = screenSize.width;
+        int height  = screenSize.height;
+
+
+        EventQueue.invokeLater(()->{//we should initialize/configure all components in EvenQueue instead of outside (refer : horstman pg no 545)
+            f.setVisible(true);
+            f.setSize(width/2, height/2);
+        });
+
+        System.out.println("f.getTitle() : " + f.getTitle());
+        System.out.println("f.ifLocationByPlatform() : " + f.getTitle());
+
+        
+        
+        //System.out.println("width  : " + Toolkit.getDefaultToolkit().getScreenSize().width);
+        //System.out.println("height : " + Toolkit.getDefaultToolkit().getScreenSize().height);
+        //   or
+        System.out.println("width  : " + width);
+        System.out.println("height : " + height);
+        System.out.println("End of main");
     }
 }
