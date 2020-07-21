4c81bec63226a33acdb64895d82bcc8ad09341e3
Wasit Shafi <wasitshafi700@gmail.com>
2020-07-21 22:50:30 +0530

    Add files via upload

---
+++ III-sem/JAVA/Practice/Basic/FullPackageName.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class FullPackageName
+{
+    public static void main(String[] args)
+    {
+        java.util.HashSet<Integer> s = new java.util.HashSet<>();
+
+        s.add(10);
+        s.add(20);
+        s.add(30);
+        System.out.println(s);
+        System.out.println("Happy Coding... :)");
+    }
+}
---
+++ III-sem/JAVA/Practice/Basic/ScannerFullPackageName.java
@@ -0,0 +1,10 @@
+public class ScannerFullPackageName
+{
+    public static void main(String args[])
+    {
+        System.out.print("Enter value of n... ");
+        int n = (new java.util.Scanner(System.in)).nextInt();
+
+        System.out.println("n :" + n);
+    }
+}
