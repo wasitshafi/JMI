9d5ced75fce0f0a980f6c05bb7e82f1ceccf4a67
Wasit Shafi <wasitshafi700@gmail.com>
2020-08-02 00:28:46 +0530

    Update quickSqort.cpp

--- III-sem/ADA/quickSqort.cpp
+++ III-sem/ADA/quickSqort.cpp
@@ -6,6 +6,38 @@
 #include<iostream>
 using namespace std;
 
+/*
+// Added on 02-08-2020
+// This is much simpler as compared to below in c++.
+ static int partition(int arr[], int low, int high)
+    {
+        int i = low - 1, temp;
+        for (int j = low ; j < high ; j++)
+            if(arr[j] <= arr[high]) // arr[high] is pivot element
+            {
+                temp = arr[++i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return  i + 1;
+    }
+
+    static void quickSort(int arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            int partition_index = partition(arr, low, high);
+            quickSort(arr, low, partition_index - 1);
+            quickSort(arr, partition_index + 1, high);
+        }
+    }
+*/
+
 int partition(int arr[], int lb, int ub)
 {
         int pivot = arr[lb];
