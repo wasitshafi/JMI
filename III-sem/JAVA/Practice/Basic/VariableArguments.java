95f5c7b582162dcf574e0a5b048819209c70244a
WasitShafi <wasitshafi700@gmail.com>
2020-10-20 12:09:17 +0530

    added new files

--- III-sem/JAVA/Practice/Basic/VariableArguments.java
+++ III-sem/JAVA/Practice/Basic/VariableArguments.java
@@ -4,24 +4,24 @@
 //program to implement variable agruments function in java
 //
 // Ref: p.no-> 141 E Balagurusamy 
-public class variableArguments
+public class VariableArguments
 {
-    public void printString(String...s)
+    public static void printString(String...s)
     {
         // printing using for each loop
         for(String i : s)
             System.out.print(i + " ");
     }
-    public void printInt(String m , int ... args)
+
+    public static void printInt(String m , int ... args)
     {
         System.out.print(m);
         for(int i : args)
             System.out.print(i + " ");
     }
+
     public static void main(String[] args)
     {
-        variableArguments obj = new variableArguments();
-
         String s1 = "The";
         String s2 = "quick";
         String s3 = "brown";
@@ -32,10 +32,9 @@ public class variableArguments
         String s8 = "the";
         String s9 = "lazy";
         String s10 = "dog.";
-
+        
         System.out.print("Strings are as : ");
-        obj.printString(s1, s2, s3, s4, s5, s6, s7, s8,s9, s10);
-
-        obj.printInt("\nIntegers are as : ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        printString(s1, s2, s3, s4, s5, s6, s7, s8,s9, s10);
+        printInt("\nIntegers are as : ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     }
 }
